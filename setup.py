941583d57e1b83a609efb1d74b50612f7dda60db
Ricardo Bánffy <rbanffy@gmail.com>
2016-11-05 11:32:14 +0000

    Bumb version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='pip-chill',
-    version='0.1.3',
+    version='0.1.4',
     description="Like `pip freeze` but lists only the packages that are not "
     "dependencies of installed packages.",
     long_description=readme + '\n\n' + history,
