620736e56d8a890429bc221a608755a9014e2b14
Ricardo Bánffy <rbanffy@gmail.com>
2016-11-05 15:22:31 +0000

    Fixing tests for an active dev virtualenv

--- tests/test_pip_chill.py
+++ tests/test_pip_chill.py
@@ -26,24 +26,45 @@ class TestPip_chill(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_pip_ommitted(self):
-        self.assertNotIn('pip', pip_chill.chill())
+    def test_pip_ommitted(self):
+        installed_packages = [
+            package for (package, version) in pip_chill.chill()]
+        for package in ['pip-chill', 'wheel', 'setuptools', 'pip']:
+            assert package not in installed_packages
 
-    def test_command_line_interface(self):
+    def test_all(self):
+        installed_packages = [
+            package for (package, version) in pip_chill.chill(True)]
+        for package in ['pip-chill', 'wheel', 'setuptools', 'pip']:
+            assert package in installed_packages
+
+    def test_command_line_interface_help(self):
         runner = CliRunner()
-        result = runner.invoke(cli.main)
+        result = runner.invoke(cli.main, ['--help'])
         assert result.exit_code == 0
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--no-version  Omit version numbers' in help_result.output
-        assert '--help' in help_result.output
-        assert 'Show this message and exit.' in help_result.output
+        assert '--no-version  Omit version numbers' in result.output
+        assert '--help' in result.output
+        assert 'Show this message and exit.' in result.output
 
     def test_command_line_interface_no_version(self):
         runner = CliRunner()
         result = runner.invoke(cli.main, ['--no-version'])
         assert result.exit_code == 0
         assert '==' not in result.output
+
+    def test_command_line_interface_omits_ignored(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        for package in ['pip-chill', 'wheel', 'setuptools', 'pip']:
+            assert package not in result.output
+
+    def test_command_line_interface_all(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.main, ['--all'])
+        assert result.exit_code == 0
+        for package in ['pip-chill', 'wheel', 'setuptools', 'pip']:
+            assert package in result.output
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
