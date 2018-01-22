35e9c5f81b5e0924eecf6c9c534543a6d38e6c8a
Ricardo Bánffy <rbanffy@gmail.com>
2018-01-22 14:25:41 +0000

    Version bump to 0.1.7
    * Update requirements
    * Add Luke Skibinski, Piotr Surowiec to authors
    * Update requirement inside setup.py - remove version pinning
    * Move Python 3 support from Python 3.5 to Python 3.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
+    'click',
 ]
 
 test_requirements = [
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='pip-chill',
-    version='0.1.6',
+    version='0.1.7',
     description="Like `pip freeze` but lists only the packages that are not "
     "dependencies of installed packages.",
     long_description=readme + '\n\n' + history,
