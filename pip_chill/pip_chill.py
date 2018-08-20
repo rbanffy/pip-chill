cab47b9a9c533b78168320b2b53df7a7b83d5032
Ricardo Bánffy <rbanffy@gmail.com>
2018-08-20 09:35:01 +0100

    Move Distribution definition inside main module

--- pip_chill/pip_chill.py
+++ pip_chill/pip_chill.py
@@ -3,7 +3,37 @@
 
 from pip._internal.utils.misc import get_installed_distributions
 
-from utils import Distribution
+
+# from pip_chill.utils import Distribution
+class Distribution:
+    def __init__(self, name, version=None, required_by=None):
+        self.name = name
+        self.version = version
+        self.required_by = set(required_by) if required_by else set()
+
+    def get_name_without_version(self):
+        if self.required_by:
+            return '# {} # Installed as dependency for {}' \
+                .format(self.name, ', '.join(self.required_by))
+        return self.name
+
+    def __str__(self):
+        if self.required_by:
+            return '# {}=={} # Installed as dependency for {}' \
+                .format(self.name, self.version, ', '.join(self.required_by))
+        return '{}=={}'.format(self.name, self.version)
+
+    def __cmp__(self, other):
+        if isinstance(other, Distribution):
+            return self.name == other.name
+
+        return self.name == other
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __hash__(self):
+        return self.name
 
 
 def chill(show_all=False):
