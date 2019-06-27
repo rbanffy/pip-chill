5207c835b56d3106b8c2b7526aad6e5d23b4f6f5
Yoni Lavi <yoni@lavi.fm>
2019-06-27 03:39:19 +0100

    Implement showing url for editable packages
    also comments and minor styling suggestions

--- pip_chill/pip_chill.py
+++ pip_chill/pip_chill.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """Lists installed packages that are not dependencies of others"""
 
+from pip._internal.operations.freeze import get_requirement_info
 from pip._internal.utils.misc import get_installed_distributions
 
 
-# from pip_chill.utils import Distribution
 class Distribution:
     def __init__(self, name, version=None, required_by=None):
         self.name = name
         self.version = version
-        self.required_by = set(required_by) if required_by else set()
+        self.required_by = set(required_by or [])
 
     def get_name_without_version(self):
         if self.required_by:
@@ -27,9 +27,9 @@ class Distribution:
         if self is other:
             return True
         elif isinstance(other, Distribution):
-            return True if self.name == other.name else False
+            return self.name == other.name
         else:
-            return True if self.name == other else False
+            return self.name == other
 
     def __lt__(self, other):
         return self.name < other.name
@@ -40,24 +40,30 @@ class Distribution:
 
 def chill(show_all=False):
     if show_all:
-        ignored_packages = ()
+        ignored_packages = set()
     else:
-        ignored_packages = {
-            'pip', 'wheel', 'setuptools', 'pkg-resources'}
+        ignored_packages = {'pip', 'wheel', 'setuptools', 'pkg-resources'}
 
     # Gather all packages that are requirements and will be auto-installed.
-    distributions = {}
-    dependencies = {}
+    distributions = {}  # mapping from package to version
+    dependencies = {}  # mapping from package to its dependencies
 
     for distribution in get_installed_distributions():
         if distribution.key in ignored_packages:
             continue
 
+        req, editable, _ = get_requirement_info(distribution)
+
+        if editable:
+            version = '{} -e {}'.format(distribution.version, req)
+        else:
+            version = distribution.version
+
         if distribution.key in dependencies:
-            dependencies[distribution.key].version = distribution.version
+            dependencies[distribution.key].version = version
         else:
             distributions[distribution.key] = \
-                Distribution(distribution.key, distribution.version)
+                Distribution(distribution.key, version)
 
         for requirement in distribution.requires():
             if requirement.key not in ignored_packages:
@@ -69,6 +75,8 @@ def chill(show_all=False):
                         requirement.key,
                         required_by=(distribution.key,))
 
+            # if the package is already in distributions, remove it
+            # and move it into the dependencies
             if requirement.key in distributions:
                 dependencies[requirement.key].version \
                     = distributions.pop(requirement.key).version
