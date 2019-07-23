e88867ffbe2432af7f8a51084fab9688c23307ce
Ricardo Bánffy <rbanffy@gmail.com>
2019-07-23 12:11:49 +0100

    Version bump, __all__ must contain a list of strings (according to PyLint)

--- pip_chill/__init__.py
+++ pip_chill/__init__.py
@@ -4,6 +4,7 @@ from .pip_chill import chill
 
 __author__ = 'Ricardo Bánffy'
 __email__ = 'rbanffy@gmail.com'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
-__all__ = [chill]
+
+__all__ = [chill.__name__]
