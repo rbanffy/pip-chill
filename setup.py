b55febbf7b03bafa4133a181029813276d61be38
Ricardo Bánffy <ricardo.banffy@workday.com>
2020-02-27 11:13:13 -0500

    Update classifiers

--- setup.py
+++ setup.py
@@ -40,8 +40,9 @@ setup(
     zip_safe=False,
     keywords='pip-chill',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Environment :: Console',
         'License :: '
         'OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
@@ -52,6 +53,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development',
     ],
     test_suite='tests',
     tests_require=test_requirements
