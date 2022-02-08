743bdcbec8404863785ba630a75d9bd28151b1aa
Ricardo Bánffy <rbanffy@gmail.com>
2022-02-08 18:34:06 +0000

    Remove Python 3.5 support

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
