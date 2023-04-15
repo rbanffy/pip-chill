65d8cd8ee453fb659b675d0d71395a4a7b93f817
Ricardo Bánffy <ricardo@gmail.com>
2023-04-15 12:30:47 +0100

    Fix version numbers

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = ["pip"]
 
 setup(
     name="pip-chill",
-    version="1.0.3",
+    version="1.0.2",
     description="Like `pip freeze` but lists only the packages that are not "
     "dependencies of installed packages.",
     long_description=readme + "\n\n" + history,
