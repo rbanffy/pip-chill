cad515731aecb8d2a292aab367b62774d35585e0
Marcin <marcin.konow@lczyk.xyz>
2024-04-28 17:33:13 +0100

    use evn marker on setuptools dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "setuptools",
+    "setuptools; python_version >= '3.12'",
 ]
 
 test_requirements = ["pip"]
