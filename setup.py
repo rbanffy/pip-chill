a87fad2500e1b12c8ee46bf754adde76ed509307
Marcin <marcin.konow@lczyk.xyz>
2024-04-28 17:23:28 +0100

    make License one string

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Environment :: Console",
-        "License :: "
-        "OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
