93b3ea7d5036cc1d8b7a844a7435cb0a4c9757f3
Marcin <marcin.konow@lczyk.xyz>
2024-04-28 17:49:56 +0100

    formating

--- tests/test_pip_chill.py
+++ tests/test_pip_chill.py
@@ -8,7 +8,6 @@ test_pip_chill
 Tests for `pip_chill` module.
 """
 
-
 import os
 import sys
 import unittest
@@ -20,12 +19,8 @@ from pip_chill.pip_chill import Distribution
 class TestPip_chill(unittest.TestCase):
     def setUp(self):
         self.distribution_1 = Distribution("pip-chill", "2.0.0", [])
-        self.distribution_2 = Distribution(
-            "pip", "10.0.0", [self.distribution_1]
-        )
-        self.distribution_3 = Distribution(
-            "pip", "11.0.0", [self.distribution_1]
-        )
+        self.distribution_2 = Distribution("pip", "10.0.0", [self.distribution_1])
+        self.distribution_3 = Distribution("pip", "11.0.0", [self.distribution_1])
 
     def tearDown(self):
         pass
@@ -110,7 +105,9 @@ class TestPip_chill(unittest.TestCase):
 
         result = os.popen(command).read()
         for package in ["wheel", "setuptools", "pip"]:
-            self.assertFalse(any(p.startswith(f"{package}==") for p in result.split("\n")))
+            self.assertFalse(
+                any(p.startswith(f"{package}==") for p in result.split("\n"))
+            )
 
     def test_command_line_interface_all(self):
         command = "pip_chill/cli.py --all"
